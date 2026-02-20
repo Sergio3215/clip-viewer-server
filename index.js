e3d5fac61cfb8914ce0b38e8b2f9e65a341cd4d0
Sergio <principiante.en.programar@gmail.com>
2026-02-20 03:20:06 -0300

    init repository

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+require("dotenv").config();
+
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+}));
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+app.post("/api/clip/", (req, res) => {
+    let { clipId, channel, raider, clipTitle, bgColor } = req.body;
+    // console.log("ClipID", clipId);
+
+    if (bgColor == undefined || bgColor == null) {
+        bgColor = "slate";
+    }
+
+    io.emit("clip", { clipId, channel, raider, clipTitle, bgColor });
+
+    if (clipId) {
+        res.send(`ClipID ${clipId} enviado`);
+    } else {
+        res.send("No clip ID");
+    }
+
+});
+
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("listening on *:" + (process.env.PORT || 3000));
+});
+
+//Websocket
+io.on("connection", (socket) => {
+    console.log("✅ Nuevo widget conectado:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("❌ Widget desconectado");
+    });
+});
+
